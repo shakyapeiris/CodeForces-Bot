6a5c49614fccd7c4abf3f4e41620b19432ccf546
shakyapeiris <shakyaimanjith32@gmail.com>
2022-10-12 23:06:24 +0530

    Update initialization script

--- init.bash
+++ init.bash
@@ -2,6 +2,13 @@
 
 npm install
 
+if [[ -f "src/tracker.json" ]];
+then 
+
+echo "tracker.json already available"
+
+else
 INITIAL_JSON='{"tracker":[]}'
 touch src/tracker.json
 echo "$INITIAL_JSON" > src/tracker.json
+fi
