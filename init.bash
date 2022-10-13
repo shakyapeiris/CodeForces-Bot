c94f57702f063c10905ddd12ac72238a50649fe8
shakyapeiris <shakyaimanjith32@gmail.com>
2022-10-13 08:57:12 +0530

    Set to take inputs for .env file

--- init.bash
+++ init.bash
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
 npm install
+echo -e "\n"
 
 if [[ -f "src/tracker.json" ]];
 then 
-
-echo "tracker.json already available"
-
+    echo "tracker.json already created"
+    echo -e "\n"
 else
-INITIAL_JSON='{"tracker":[]}'
-touch src/tracker.json
-echo "$INITIAL_JSON" > src/tracker.json
+    INITIAL_JSON='{"tracker":[]}'
+    touch src/tracker.json
+    echo "$INITIAL_JSON" > src/tracker.json
+fi
+
+if [[ -f ".env" ]];
+then
+    echo ".env file is already created"
+else 
+    read -sp "Enter bots token: " TOKEN
+    echo -e "\n"
+    read -p "Enter a channel ID: " CHANNEL_ID
+    echo "DISCORD_BOT_TOKEN=$TOKEN" > ".env"
+    echo "CHANNEL_ID=$CHANNEL_ID"  >> ".env"
 fi
